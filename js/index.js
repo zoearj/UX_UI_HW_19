8d320018507837ecf2734e7ee8b13c1db05f591d
zoearj <zoearj@gmail.com>
2023-05-18 17:55:14 -0700

    commi3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 console.log("Your index.js file is loaded correctly!");
+    $(document).ready(function(){
+        $("button").click(function(){
+            $(this).css("background-color","#C4DDEC");
+        });
+
+// Changes nav. .li tags color on hover //
+
+    $('#A').mouseenter(function(){
+        $(this).css('background-color', '#f7f5dc');
+    });
+  
+   
+    $('#B').mouseenter(function(){
+        $(this).css('background-color', '#f7f5dc');
+    });
+
+
+    $('#C').mouseenter(function(){
+        $(this).css('background-color', '#f7f5dc');
+    });
+
+
+    $('#D').mouseenter(function(){
+        $(this).css('background-color', '#f7f5dc');
+    });
+
+    // changes the nav. .li tags color back to original//
+   
+    $('#A').mouseout(function() {
+        $(this).css('originalStyles');
+    });
+   
+    //scroll behavior //
+    });
